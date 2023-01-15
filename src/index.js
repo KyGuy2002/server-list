927601e40c6d101e27edcd92e003ed0d404989f8
KyGuy2002 <kylanjaeger@gmail.com>
2023-01-14 18:31:09 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/index.scss';
+import Game from './components/Game.js';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    // Header
+    <Game/>
+    // Footer
+);
