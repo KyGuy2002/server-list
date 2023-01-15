4873da4759622e980fa31cba3bd2973e1cd4eada
KyGuy2002 <kylanjaeger@gmail.com>
2023-01-14 19:09:34 -0800

    responsive

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/header.scss';
+
+export default function Header() {
+    return (
+        <header>
+
+            <h1>Tic - Tac - Toe</h1>
+
+        </header>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
 import Game from './components/Game.js';
+import Header from './components/Header.js';
 
+
+function Root() {
+    return (<>
+        <Header/>
+        <Game/>
+    </>)
+}
+
+
+// Render
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    // Header
-    <Game/>
-    // Footer
-);
+root.render (<Root/>)
